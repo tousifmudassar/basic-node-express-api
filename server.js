2d60377c9c37b6533c037346fd9004dc5257f056
Mudassar Tousif <tousifmudassar@gmail.com>
2020-12-17 16:41:29 +0530

    Created a basic Express JS Web Server.

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json("Hello GeeksforGeeks!");
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}.`);
+});
