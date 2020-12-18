391029ef0f24a1f9833cbb7b168a18db6ed7300c
Mudassar Tousif <tousifmudassar@gmail.com>
2020-12-18 12:38:26 +0530

    Implemented single user.

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -3,7 +3,14 @@ const todos = require("../../constants/todos");
 const app = express.Router();
 
 app.get("/", (req, res) => {
-  res.json("Welcome to Todos!");
+  res.json(todos);
 });
-
+app.get("/:id", (req, res) => {
+  const id = +req.params.id;
+  if (!todos[id]) {
+    res.status(404).json("Error! Todo Not Found!");
+  } else {
+    res.status(202).json(todos[id]);
+  }
+});
 module.exports = app;
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,20 @@ const users = require("../../constants/users");
 const app = express.Router();
 
 app.get("/", (req, res) => {
-  res.json("Welcome to Users!");
+  res.json(
+    users.map(u => {
+      const a = { ...u };
+      delete a.Password;
+      return a;
+    })
+  );
 });
-
+app.get("/:id", (req, res) => {
+  const id = +req.params.id;
+  if (!users[id]) {
+    res.status(404).json("Error! Todo Not Found!");
+  } else {
+    res.status(202).json(users[id]);
+  }
+});
 module.exports = app;
