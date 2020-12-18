f5dcd8c6165b12a9ea120934e9a6c1dc3832a8fd
Mudassar Tousif <tousifmudassar@gmail.com>
2020-12-18 08:54:34 +0530

    Implemented a moduler routing technique.

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const root = require("./routes/root");
 const app = express();
 const port = 3000;
+// const todos = ["Milk", "Curd", "Sugar"];
 
-app.get("/", (req, res) => {
-  res.json("Hello, GeeksforGeeks API Server!");
-});
+app.use("/", root);
+
+// app.get("/", (req, res) => {
+//   res.json("Hello, GeeksforGeeks API Server!");
+// });
+// app.get("/api/", (req, res) => {
+//   res.json("This is the root of API Server!");
+// });
+// app.get("/api/todos", (req, res) => {
+//   res.json(todos);
+// });
+// app.get("/api/todos/:id", (req, res) => {
+//   const param = +req.params.id;
+//   if (!todos[param]) {
+//     res.status(404).json("Error Not Found");
+//   } else {
+//     res.json(todos[param]);
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
