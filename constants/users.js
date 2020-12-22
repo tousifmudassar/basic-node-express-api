d4722df2873bed496bc5ab210c11a2b4e33d22d1
Mudassar Tousif <tousifmudassar@gmail.com>
2020-12-22 13:47:17 +0530

    Created two dummy users with passwords.

--- constants/users.js
+++ constants/users.js
@@ -1,27 +1,34 @@
 module.exports = [
   {
+    Username: "Mudassar",
     Name: "Mudassar Tousif",
     Password: "test@123",
     LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
   },
   {
-    Name: "Mistkat Tousif",
-    Password: "test@1234",
-    LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
-  },
-  {
-    Name: "Bashira Tousif",
-    Password: "test@12345",
-    LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
-  },
-  {
-    Name: "Naseeba Tousif",
-    Password: "test@123456",
-    LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
-  },
-  {
-    Name: "John",
-    Password: "test@1234567",
-    LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
+    Username: "John",
+    Name: "John Corona",
+    Password: "corona@123",
+    LinkedIn: "https://www.linkedin.com/in/md-covid19-7b5775166/"
   }
+  // {
+  //   Name: "Mistkat Tousif",
+  //   Password: "test@1234",
+  //   LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
+  // },
+  // {
+  //   Name: "Bashira Tousif",
+  //   Password: "test@12345",
+  //   LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
+  // },
+  // {
+  //   Name: "Naseeba Tousif",
+  //   Password: "test@123456",
+  //   LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
+  // },
+  // {
+  //   Name: "John",
+  //   Password: "test@1234567",
+  //   LinkedIn: "https://www.linkedin.com/in/md-tousif-7b5775166/"
+  // }
 ];
